8499f107d2cad16c3bc32712ef21a1ad9cc37fa4
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-08-14 13:37:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -157,9 +157,6 @@ async function executarScanner() {
                                                 totalMatchesPalavras = 0; 
                                                 totalReversoesCandle = 0;
                                                 totalMatchesFonteSecundaria = 0;
-                                                
-                                                // A data legível para salvar no histórico depois
-                                                const dataDesseDia = new Date(timestampDoDia * 1000).toLocaleDateString('pt-BR');
                                     
                                                 // ==========================================
                                                 // Agora as 50 ações entram para trabalhar:
