3765ec421cb63c41325114aa749891563ed68d73
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-08-29 10:09:22 -0300

    Remove console log and return from app.js
    Removed console log and return statement from processing logic.

--- app.js
+++ app.js
@@ -128,8 +128,7 @@ async function executarScanner() {
                                     let historicoReatualizado;
                                     let historicoSalvo = JSON.parse(localStorage.getItem('historico_B3')) || [];
                                     historicoReatualizado = structuredClone(historicoSalvo);
-                                    console.log(historicoReatualizado);
-                                    return; 
+                                    
                                     diasParaProcessar.forEach((diaDoCalendario) => {
                                                 
                                                 
