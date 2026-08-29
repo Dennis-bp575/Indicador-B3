63c1e9d9114f804e5fe7479581cfdacc9ccd078d
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-08-29 17:26:00 -0300

    Log diasParaProcessar to console

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ async function executarScanner() {
                                 
                                     // Criamos a lista com os dias que faltam processar dali para frente!
                                     const diasParaProcessar = historicoCalendario.slice(indiceParada);
-                                    
+                                    console.log(diasParaProcessar);
                                     //let historicoReatualizado;
                                     let historicoSalvo = JSON.parse(localStorage.getItem('historico_B3')) || [];
                                     //historicoReatualizado = structuredClone(historicoSalvo);
