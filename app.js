cba75123e21824582fd4d62c0c73fbae07b89a6f
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-08-29 11:24:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -179,8 +179,7 @@ async function executarScanner() {
                                    
                                     // Criamos a lista com os dias que faltam processar dali para frente!
                                     const diasParaProcessar = historicoCalendario.slice(indiceParada);
-                                    console.log(diasParaProcessar)
-                                    return;
+                                    
                                     //let historicoReatualizado;
                                     let historicoSalvo = JSON.parse(localStorage.getItem('historico_B3')) || [];
                                     //historicoReatualizado = structuredClone(historicoSalvo);
