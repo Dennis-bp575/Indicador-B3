1640c8ff7ec11bb64114748a811bd3f37cc3b9c7
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-08-29 11:40:59 -0300

    Remove console logs for data processing

--- app.js
+++ app.js
@@ -192,17 +192,16 @@ async function executarScanner() {
                                                 const dataDesseDia = new Date(timestampDoDia * 1000).toLocaleDateString('pt-BR');
                                                 //console.log(dataDesseDia)
                                                 dataDesseDiaGlobal = dataDesseDia;
-                                                console.log(dataDesseDia);
+                                                
                                                 let registroExistente = historicoReatualizado.find(item => 
                                                             item && String(item.dataSinal).trim() === String(dataDesseDia).trim()
                                                 );
 
-                                                console.log(registroExistente);
+                                                
                                                 if (registroExistente) {
         
                                                     const dadosIbov = dadosBrutos.results.find(item => item.symbol === "IBOV");
-                                                            console.log(dadosIbov);
-                                                            return;
+                                                            
                                                     if (dadosIbov && dadosIbov.historicalDataPrice) {
                                                         const historicoIbov = dadosIbov.historicalDataPrice;
                                                         // Achamos a posição do dia anterior no histórico do Ibov
@@ -252,7 +251,7 @@ async function executarScanner() {
                                                         }
                                                     }
                                                 }   
-                                                return;
+                                                
                                     });
 
                                     diasParaProcessar.forEach((diaDoCalendario) => {
