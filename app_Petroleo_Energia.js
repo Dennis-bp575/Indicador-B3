3b7aef5ec468f2d0cd379315cb9fd918b408b6a3
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-09-17 00:12:00 -0300

    Uncomment console.log for debugging purposes

--- app_Petroleo_Energia.js
+++ app_Petroleo_Energia.js
@@ -604,7 +604,7 @@ function desenharHistoricoNaTela() {
             // ... dentro do seu laço de repetição (forEach ou for) ...
             
             if (etiquetaSinal !== "[⚪NEUTRO]") {
-                // console.log(etiquetaSinal, ehLong, ehShort, ehSdShort);
+                console.log(etiquetaSinal, ehLong, ehShort, ehSdShort);
                         // return;
                 // Define uma variável para controlar se o sinal atual é válido baseado no histórico
                 let sinalValido = false;
