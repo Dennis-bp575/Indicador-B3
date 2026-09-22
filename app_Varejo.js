952889ecc453bb275ee6142e99d03dcc813936dc
Dennis-bp575 <dennis.ubaldo@gmail.com>
2026-09-22 09:13:49 -0300

    Refactor condition variables in app_Varejo.js

--- app_Varejo.js
+++ app_Varejo.js
@@ -430,9 +430,9 @@ async function executarScanner() {
                 condicaoA = numA <= 2;
                 condicaoB = somaBC >= 35;
                 condicaoC = somaBC <= 48;
-            const condicaoA_S =numA <= 3;
-            const condicaoB_S = somaBC >= 33;
-            const condicaoC_S = somaBC <= 50;         
+            condicaoA_S =numA <= 3;
+            condicaoB_S = somaBC >= 33;
+            condicaoC_S = somaBC <= 50;         
 
             if (condicaoA && condicaoB && condicaoC) {
                 // SINAL PRINCIPAL (Forte)
@@ -454,8 +454,8 @@ async function executarScanner() {
 
             condicaoA = numA <= 2;
             condicaoB = somaBC > 49;
-            const condicaoA_S =numA <= 3;
-            const condicaoB_S = somaBC > 47;
+            condicaoA_S =numA <= 3;
+            condicaoB_S = somaBC > 47;
 
             if (condicaoA && condicaoB) {
                 // SINAL PRINCIPAL (Forte)
